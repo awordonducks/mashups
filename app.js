5a4c9bcbc23d534d7023d9b1449682abb9988823
awordonducks <jameswfhosken@gmail.com>
2014-11-18 17:41:51 +0400

    Add server listen

--- app.js
+++ app.js
@@ -20,3 +20,13 @@ app.get("*", function(req, res){
 app.listen(3000){
 	console.log("Express started on 3000, woo!");
 }
+
+
+//SERVER START
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
